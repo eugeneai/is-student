666669cf05dd73c0c8f172dc180f2e59f49dc967
Evgeny Cherkashin <eugeneai@irnok.net>
2016-03-21 19:28:01 +0800

    Adjusted static routes

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from bottle import *
+import os, os.path
+
+CURRD=os.getcwd()
 
 @route("/")
 @view("main.pt")
 def index():
-    return {"name":name, "phone":"+7 (914) 870 67-54"}
+    return {"name":"Stranger", "phone":"+7 (914) 870 67-54"}
 
 @route('/hello/<name>')
 @view('main.pt')
@@ -14,7 +17,17 @@ def hello(name):
 @route('/static/<filename:path>')
 def send_static(filename):
     return static_file(filename,
-        root='/home/stud/Example/2016-appl/is-student/static')
+        root=os.path.join(CURRD,'static'))
+
+@route('/img/<filename:path>')
+def send_static(filename):
+    return static_file(filename,
+        root=os.path.join(CURRD,'static','img'))
+
+@route('/font-awesome/<filename:path>')
+def send_static(filename):
+    return static_file(filename,
+        root=os.path.join(CURRD,'static','font-awesome'))
 
 # print(TEMPLATE_PATH)
 
